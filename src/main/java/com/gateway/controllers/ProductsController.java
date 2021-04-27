3e8110f2adf2fdeb8f8cf9ef1ca62a994bee4fb7
Paweł Kudłacik <pakudlacik@gmail.com>
2021-04-27 23:33:32 +0200

    Add trailing slashes to ProductsController

--- src/main/java/com/gateway/controllers/ProductsController.java
+++ src/main/java/com/gateway/controllers/ProductsController.java
@@ -2,13 +2,15 @@ package com.gateway.controllers;
 
 import org.springframework.beans.factory.annotation.Value;
 import org.springframework.security.access.annotation.Secured;
-import org.springframework.web.bind.annotation.*;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestMethod;
+import org.springframework.web.bind.annotation.RestController;
 
 import javax.servlet.http.HttpServletRequest;
 import javax.servlet.http.HttpServletResponse;
 import java.net.URI;
 import java.net.URISyntaxException;
-import java.security.Principal;
 
 @SuppressWarnings("MVCPathVariableInspection")
 @RestController
@@ -20,13 +22,13 @@ public class ProductsController extends GatewayController {
         super(new URI(productsServiceUrl));
     }
 
-    @GetMapping(value = {"/products", "/products/{id}"})
+    @GetMapping(value = {"/products/", "/products/{id}/"})
     public void HandlePublicRequests(HttpServletRequest req, HttpServletResponse resp){
         forwardRequest(req, resp);
     }
 
     @RequestMapping(
-            value = {"/products", "/products/{id}"},
+            value = {"/products/", "/products/{id}/"},
             method = {RequestMethod.POST,RequestMethod.PUT, RequestMethod.DELETE})
     @Secured("ROLE_ADMIN")
     public void HandleAdminRequests(HttpServletRequest req, HttpServletResponse resp){
